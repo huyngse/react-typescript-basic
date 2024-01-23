bd59e76c086ed17ad61c063fffb49af53e317290
huyngse <ngogiahuy775309@gmail.com>
2024-01-23 10:45:28 +0700

    Initial commit

---
+++ src/context/ProductsProvider.tsx
@@ -0,0 +1,53 @@
+import { ReactElement, createContext, useState, useEffect } from "react";
+
+export type ProductType = {
+  sku: string;
+  name: string;
+  price: number;
+};
+export type UseProductContextType = {
+  products: ProductType[];
+};
+// const initState: ProductType[] = [];
+const initState: ProductType[] = [
+  {
+    sku: "item0001",
+    name: "Widget",
+    price: 9.99,
+  },
+  {
+    sku: "item0002",
+    name: "Premium Widget",
+    price: 19.99,
+  },
+  {
+    sku: "item0003",
+    name: "Deluxe Widget",
+    price: 29.99,
+  },
+];
+const initContextState: UseProductContextType = {
+  products: [],
+};
+const ProductsContext = createContext<UseProductContextType>(initContextState);
+type ChildrenType = {
+  children?: ReactElement | ReactElement[];
+};
+export const ProductsProvider = ({ children }: ChildrenType): ReactElement => {
+  const [products, setProducts] = useState<ProductType[]>(initState);
+  // useEffect(() => {
+  //   const fetchProducts = async (): Promise<ProductType[]> => {
+  //     const data await fetch();
+  //   }
+  // }, []);
+  return (
+    <ProductsContext.Provider
+      value={{
+        products,
+      }}
+    >
+      {children}
+    </ProductsContext.Provider>
+  );
+};
+export default ProductsContext;
